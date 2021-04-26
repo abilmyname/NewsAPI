9132a85aeed69db19cb7da2a3b571eeeede7db1d
abilmyname <abilnabilachmad@gmail.com>
2021-04-26 22:03:46 +0700

    first commit

---
+++ NewsAPI/Favorite/FavoriteNewsDetail.swift
@@ -0,0 +1,20 @@
+//
+//  FavoriteNewsDetail.swift
+//  NewsAPI
+//
+//  Created by Apple on 20/04/21.
+//
+
+import SwiftUI
+
+struct FavoriteNewsDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoriteNewsDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteNewsDetail()
+    }
+}
